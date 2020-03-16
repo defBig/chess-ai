49a69a41dfe727b4046b108e4efccbb93916e699
lightTuring <cloud.losing@gmail.com>
2020-03-16 20:05:26 -0300

    Classe Graph modificada

--- AI/src/Algorithm/Graph.java
+++ AI/src/Algorithm/Graph.java
@@ -2,38 +2,39 @@ package Algorithm;
 
 import java.util.ArrayList;
 
+import Rules.Board;
+
+class Node {
+	int id;
+	int weight;
+	Board board;
+
+	Node (int id, int w, Board b) {
+		weight = w;
+		board = b;
+	}
+	
+	
+}
+
 public class Graph {
 
-	private ArrayList<Integer>[] graph;
+	private ArrayList<Node> graph;
+	private ArrayList<ArrayList<Integer>> adj;
 	
-	public Graph(int depth) {
-		this.graph = new ArrayList[depth];
-		for (int i = 0; i < graph.length; i++) {
-			this.graph[i] = new ArrayList<Integer>();
-		}
+	public void createNode(int id, int w, Board b) {
+		Node n = new Node (id, w, b);
+		graph.add(n);
+
+	}
+
+	public void addEdge (Node a, Node b) {
+		int i = graph.indexOf(a);
+		int j = graph.indexOf(b);
+		adj.get(i).add(j);
 	}
 	
-	public void addElements(int node, int ... elements) {
-		for (int i : elements) {
-			this.graph[node].add(i);
-		}
-	}
 	
-	public ArrayList<Integer>[] graph(){
-		return this.graph;
-	}
-	/*
-	public static void main(String[] args) {
-		Graph g = new Graph(2);
-		g.addElements(0, 1,2,2,3,4,5);
-		g.addElements(1, 2,3,4,5);
-		
-		ArrayList<Integer>[] graph = g.graph();
-		for (int i = 0; i < graph.length; i++) {
-			for (int j = 0; j < graph[i].size(); j++) {
-				System.out.print(graph[i].get(j));
-			}
-			System.out.println();
-		}
-	}*/
+	
+
 }
